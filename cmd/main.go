eeedc66e36c8cb828a85af59c16784bf1405fee8
Home <ice_rain2009@mail.ru>
2023-09-20 17:10:25 +0300

    created server

---
+++ cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	restapiauthentication "Rest_api_authentication"
+	"Rest_api_authentication/pkg/handler"
+	"log"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(restapiauthentication.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+
+}
