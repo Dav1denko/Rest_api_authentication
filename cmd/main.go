45338f26d193fd718b01c69c00b211b1a8ff2eb4
Home <ice_rain2009@mail.ru>
2023-09-21 16:14:27 +0300

    initConfig

--- cmd/main.go
+++ cmd/main.go
@@ -6,16 +6,27 @@ import (
 	"Rest_api_authentication/pkg/repository"
 	"Rest_api_authentication/pkg/service"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error init configs %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(restapiauthentication.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
